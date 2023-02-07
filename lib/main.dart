411ac8bdd23601afe0cff62fe9255958ae01e2ff
Timothy Peter <9321819+cuskar@users.noreply.github.com>
2023-02-07 09:38:00 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,17 @@ import 'package:final_project/view/main_page.dart';
 import 'package:final_project/view/paket_soal_page.dart';
 import 'package:final_project/view/register_page.dart';
 import 'package:final_project/view/splash_screen.dart';
-import 'package:final_project/constants/R/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:final_project/constants/R/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
+
   runApp(const MyApp());
 }
 
